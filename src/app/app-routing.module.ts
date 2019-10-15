9d8108986740a512360f5d9ce7c1f8bf6a515e54
Ismail NORRI <ismail.norri.24@gmail.com>
2019-10-15 16:30:34 +0000

    Configuration - Routing - création compo & service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { ListClientsComponent } from './list-clients/list-clients.component';
+import { FormClientComponent } from './list-clients/form-client/form-client.component';
+import { ListCompteClientComponent } from './list-clients/list-compte-client/list-compte-client.component';
+import { VirementComponent } from './virement/virement/virement.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auth/login', component: LoginComponent },
+  {path: 'list-clients', component: ListClientsComponent },
+  {path: 'list-clients/new-client', component: FormClientComponent},
+  {path: 'list-clients/update-client', component: FormClientComponent},
+  {path: 'list-clients/list-compte-client', component: ListCompteClientComponent},
+  {path: 'virement/virement', component: VirementComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ListClientsComponent } from './list-clients/list-clients.component';
+import { ListCompteClientComponent } from './list-clients/list-compte-client/list-compte-client.component';
+import { FormClientComponent } from './list-clients/form-client/form-client.component';
+import { HeaderComponent } from './header/header.component';
+import { VirementComponent } from './virement/virement/virement.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ListClientsComponent,
+    ListCompteClientComponent,
+    FormClientComponent,
+    HeaderComponent,
+    VirementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
